86a321e887f5c25c88d96f8dda6dcaaaef856e8e
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2019-03-03 16:42:25 -0800

    Create TestingQueries.sql

---
+++ sql/TestingQueries.sql
@@ -0,0 +1,11 @@
+﻿--SELECT * FROM dbo.Items
+--SELECT * FROM dbo.Tags
+
+SELECT Name,Quantity,Row,Col FROM dbo.Items WHERE Items.NameKey LIKE 'aa battery'
+
+--update dbo.Tags
+--set NameKey = LOWER(Name)
+
+
+--update dbo.Items
+--set NameKey = LOWER(Name)
