2f78d95737f710b14ca65a44dbb1e0093ac17064
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2019-04-02 02:09:56 -0700

    Update CreateTables.sql

--- sql/CreateTables.sql
+++ sql/CreateTables.sql
@@ -1,34 +1,38 @@
-CREATE TABLE [dbo].[Item]
-(
-    [Name] NVARCHAR(50) NOT NULL, 
-    [Quantity] INT NOT NULL DEFAULT 0, 
-    [TimeCreated] DATETIME NOT NULL, 
-    [Row] INT NOT NULL, 
-    [Col] INT NOT NULL, 
-    [LastUpdated] DATETIME NULL, 
-    PRIMARY KEY ([Name]) 
-)
+CREATE TABLE [dbo].[Items] (
+    [NameKey]     VARCHAR (50) NOT NULL,
+    [Name]        VARCHAR (50) NOT NULL,
+    [Quantity]    INT          DEFAULT ((0)) NOT NULL,
+    [Row]         INT          NOT NULL,
+    [Col]         INT          NOT NULL,
+    [IsSmallBox]  BIT          DEFAULT ((1)) NOT NULL,
+    [DateCreated] DATETIME     NOT NULL,
+    [LastUpdated] DATETIME     NULL,
+    PRIMARY KEY CLUSTERED ([NameKey] ASC)
+);
 
-CREATE TABLE [dbo].[HttpRequests]
-(
-	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
-    [HttpRequestBody] NVARCHAR(MAX) NULL, 
-    [DateCreated] DATETIME NOT NULL
-)
 
-CREATE TABLE [dbo].[Commands]
-(
-	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
-    [DateCreated] DATETIME NOT NULL, 
-    [Command] VARCHAR(50) NOT NULL, 
-    [DataIn] VARCHAR(255) NULL, 
-    [DataOut] VARCHAR(255) NULL
-)
+CREATE TABLE [dbo].[Tags] (
+    [Id]      INT          IDENTITY (1, 1) NOT NULL,
+    [NameKey] VARCHAR (50) NOT NULL,
+    [Tag]     VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Tag_Item] FOREIGN KEY ([NameKey]) REFERENCES [dbo].[Items] ([NameKey])
+);
 
-CREATE TABLE [dbo].[Tags]
-(
-	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
-    [Name] NVARCHAR(50) NOT NULL, 
-    [Tag] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_Tag_Item] FOREIGN KEY ([Name]) REFERENCES [Item]([Name])
-)
+
+CREATE TABLE [dbo].[Commands] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [DateCreated] DATETIME      NOT NULL,
+    [Command]     VARCHAR (50)  NOT NULL,
+    [DataIn]      VARCHAR (255) NULL,
+    [DataOut]     VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+CREATE TABLE [dbo].[HttpRequests] (
+    [Id]              INT           IDENTITY (1, 1) NOT NULL,
+    [HttpRequestBody] VARCHAR (MAX) NULL,
+    [DateCreated]     DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
